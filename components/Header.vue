0b0e38c7a269c6aa6eea51a427cf280279c69f89
Jan_Lebar <janstefanlebar@gmail.com>
2023-10-30 23:36:41 +0100

    header change

--- components/Header.vue
+++ components/Header.vue
@@ -1,11 +1,30 @@
 <template>
     <header class="bg-blue-500 p-4">
       <nav class="container mx-auto flex items-center justify-between">
-        <div class="text-white text-2xl font-bold">My Nuxt App</div>
+        <nuxt-link
+            to="/" class="text-white text-2xl font-bold">Different</nuxt-link>
+
         <ul class="flex space-x-4">
-          <li><nuxt-link to="/" class="text-white hover:text-blue-300">Home</nuxt-link></li>
-          <li><nuxt-link to="/about" class="text-white hover:text-blue-300">About</nuxt-link></li>
-          <li><nuxt-link to="/contact" class="text-white hover:text-blue-300">Contact</nuxt-link></li>
+
+          <li><nuxt-link
+            to="/"
+            class="font-general-medium block text-white text-left text-lg font-medium text-primary-dark dark:text-ternary-light hover:text-indigo-600 dark:hover:text-indigo-300  sm:mx-4 mb-2 sm:py-2 border-t-2 pt-3 sm:pt-2 sm:border-t-0 border-primary-light dark:border-secondary-dark"
+            aria-label="Home"
+            >Home</nuxt-link>
+          </li>
+          <li><nuxt-link
+            to="/about"
+            class="font-general-medium block text-white text-left text-lg font-medium text-primary-dark dark:text-ternary-light hover:text-indigo-600 dark:hover:text-indigo-300  sm:mx-4 mb-2 sm:py-2 border-t-2 pt-3 sm:pt-2 sm:border-t-0 border-primary-light dark:border-secondary-dark"
+            aria-label="About Me"
+            >About Me</nuxt-link>
+          </li>
+          <li><nuxt-link
+            to="/contact"
+            class="font-general-medium block text-white text-left text-lg font-medium text-primary-dark dark:text-ternary-light hover:text-indigo-600 dark:hover:text-indigo-300  sm:mx-4 mb-2 sm:py-2 border-t-2 pt-3 sm:pt-2 sm:border-t-0 border-primary-light dark:border-secondary-dark"
+            aria-label="Contact"
+            >Contact</nuxt-link>
+          </li>
+
         </ul>
       </nav>
     </header>
