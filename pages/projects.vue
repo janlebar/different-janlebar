50e0645a9b6818636cd18e386b8beb9c150c25f4
Jan_Lebar <janstefanlebar@gmail.com>
2023-10-31 15:58:23 +0100

    ading grid for projects

---
+++ pages/projects.vue
@@ -0,0 +1,16 @@
+<script>
+import ProjectsGrid from '../components/projects/ProjectsGrid.vue';
+
+export default {
+	name: 'Projects',
+	components: {
+		ProjectsGrid,
+	},
+};
+</script>
+
+<template>
+	<div class="container mx-auto">
+		<ProjectsGrid />
+	</div>
+</template>
