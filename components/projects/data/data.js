11b9db5e55811974b1ca737e57e899a6dfc834de
JAN <janstefanlebar@gmail.com>
2023-11-06 23:34:11 +0100

    update

--- components/projects/data/data.js
+++ components/projects/data/data.js
@@ -1,9 +1,13 @@
+import img from './web-project-1.jpg';
+
+
+
 const project= [
 	{
 	  id: 1,
 	  title: 'Sideeffects app',
 	  category: 'Web Application',
-	//   img: require('./sideeffects.svg'),
+	  img: img,
 	},
   ];
   
