59e02875cf926c1b0a16ff50aacf4d642c082762
JAN <janstefanlebar@gmail.com>
2023-11-07 13:45:57 +0100

    update on image projects

--- components/projects/data/data.js
+++ components/projects/data/data.js
@@ -1,15 +1,23 @@
 const imageContext = import.meta.globEager('./project-images/*.{jpg,svg}');
 
 const projects = Object.entries(imageContext).map(([imagePath, imageImport]) => {
+  if (!imagePath) {
+    // Handle the case where imagePath is undefined (e.g., invalid path or no matching files)
+    return null;
+  }
+  
   const fileName = imagePath.replace('./project-images/', ''); // Get the file name
   const [title, category] = fileName.split('-'); // Split the file name into title and category
+
+  // Remove the file extensions (.svg or .jpg) from the category
+  const categoryWithoutExtension = category.replace(/\.(svg|jpg)/, '');
 
   return {
     id: title,
     title: title,
-    category: category, // Use the extracted category
+    category: categoryWithoutExtension, // Use the extracted category without the extension
     img: imageImport.default,
   };
-});
+}).filter(Boolean); // Remove null entries
 
 export default projects;
