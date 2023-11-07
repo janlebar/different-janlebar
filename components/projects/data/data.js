1cb8118e75712ae4480bf00678d19a9f16a783a2
JAN <janstefanlebar@gmail.com>
2023-11-07 13:30:27 +0100

    category for images from path

--- components/projects/data/data.js
+++ components/projects/data/data.js
@@ -1,15 +1,15 @@
 const imageContext = import.meta.globEager('./project-images/*.{jpg,svg}');
 
 const projects = Object.entries(imageContext).map(([imagePath, imageImport]) => {
-  const title = imagePath.replace('./project-images/', '').replace(/\.(jpg|svg)/, '');
+  const fileName = imagePath.replace('./project-images/', ''); // Get the file name
+  const [title, category] = fileName.split('-'); // Split the file name into title and category
 
   return {
     id: title,
     title: title,
-    category: 'Web Application',
+    category: category, // Use the extracted category
     img: imageImport.default,
   };
 });
 
 export default projects;
-
