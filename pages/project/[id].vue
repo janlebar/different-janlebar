acf2891648a512204ff8b18b04507ac9c34806c6
JAN <janstefanlebar@gmail.com>
2023-11-14 13:48:34 +0100

    path for dm dznamically lower case?

--- pages/project/[id].vue
+++ pages/project/[id].vue
@@ -4,7 +4,7 @@ const route = useRoute()
 
 // When accessing /posts/1, route.params.id will be 1
 console.log(route.params.id)
-
+const path = route.params.id.toLowerCase();
 
 </script>
 
@@ -14,8 +14,8 @@ console.log(route.params.id)
 
     <main>
       <!-- Use route.params.id as the target.md -->
-      <ContentDoc :path="`/projects/${route.params.id}`" />
-      <ContentDoc path="/my-post" />
+      <ContentDoc :path="`/projects/${path}`" />
+      <!-- <ContentDoc path="/my-post" /> -->
 
     </main>
   </div>
