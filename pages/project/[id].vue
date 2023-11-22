b04b6f9a1840b404d743dacb4336fc9840f6608d
JAN <janstefanlebar@gmail.com>
2023-11-22 20:09:30 +0100

    test

--- pages/project/[id].vue
+++ pages/project/[id].vue
@@ -1,39 +1,37 @@
 <script setup>
 import { useRoute } from 'vue-router'
-import { ref, watchEffect } from 'vue';
+import { ref } from 'vue';
 import Header from '~/components/Header.vue';
 
-
-
 const route = useRoute()
-
-// When accessing /posts/1, route.params.id will be 1
 console.log(route.params.id)
 const path = route.params.id.toLowerCase();
 
-console.log('here')
+
+// Create a new ref to store the description
+const filtereddescription = ref('')
+console.log(filtereddescription + 'HERE')
+console.log(filtereddescription[1] + 'HERE')
+
+// const elem = document.getElementsByClassName("strong");
+// console.log(elem + 'here strong')
+
 
 
 </script>
 
 <template>
   <div>
-    <!-- Hello {{ route.params.id }}! -->
-
-
     <main>
       <Header/>
-      <!-- <ProjectInfo></ProjectInfo> -->
-      <!-- Use route.params.id as the target.md -->
       <div class="p-20">
       <ContentDoc :path="`/singleprojects/${path}`" v-slot="{ doc }">
         <div class="container mx-auto text-center">
         <h1 class="text-4xl font-bold pt-0 p-10">{{ doc.title }}</h1>
         <h1 class="text-4xl font-bold pt-0 p-10">{{ doc.description }}</h1>
-        <!-- <img :src="doc.image"/> -->
         
-
-
+        <!-- Update the description ref when doc changes -->
+        <template v-if="filtereddescription = doc.description"></template>
 
         <div class="flex flex-wrap -mx-2">
           <div v-for="(image, index) in doc.image" :key="index" class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 p-2">
@@ -51,8 +49,9 @@ console.log('here')
       </ContentDoc>
     </div>
     <div class="pt-0">
-    <Footerprojects/>
-  </div>
+      <!-- Pass the description ref to the Footerprojects component -->
+      <Footerprojects :description="filtereddescription"/>
+    </div>
     </main>
   </div>
 
