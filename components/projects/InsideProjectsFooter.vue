c8c9695e931673ad1a3925c7a00330576987cf9b
JAN <janstefanlebar@gmail.com>
2023-11-22 23:58:56 +0100

    square 1

--- components/projects/InsideProjectsFooter.vue
+++ components/projects/InsideProjectsFooter.vue
@@ -1,7 +1,7 @@
 
 
 
-<!-- <script>
+<script>
 import feather from 'feather-icons';
 import ProjectsFilter from './ProjectsFilter.vue';
 import ProjectSingle from './ProjectSingle.vue';
@@ -14,7 +14,7 @@ export default {
 	props: ['allProjects','category','description'],
 	data: () => {
 		return {
-			FilteredCategory: description,
+			FilteredCategory: test,
 		};
 	},
 	computed: {
@@ -49,11 +49,12 @@ export default {
 	},
 };
 </script>
+ 
 
-<template>
-	Projects grid 
+ <template>
+	<!-- Projects grid  -->
 	<section class="pt-10 sm:pt-14">
-		 Filter and search projects
+		 <!-- Filter and search projects -->
 		<div class="mt-10 sm:mt-10">
 
 			<div
@@ -71,11 +72,11 @@ export default {
 			</div>
 		</div>
 
-		Projects grid 
+		<!-- Projects grid  -->
 		<div
 			class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mt-6 sm:gap-10"
 		>
-		 MAP THROUGH PROJECTS AND DISPLAY THEM
+		 <!-- MAP THROUGH PROJECTS AND DISPLAY THEM -->
 			<ProjectSingle
 				v-for="project in filteredProjects"
 				:key="project.id"
@@ -83,55 +84,7 @@ export default {
 			/>
 		</div>
 	</section>
-</template> -->
+</template> 
 
 
-<script>
-import feather from 'feather-icons';
-import ProjectsFilter from './ProjectsFilter.vue';
-import ProjectSingle from './ProjectSingle.vue';
 
-export default {
-	components: { ProjectSingle, ProjectsFilter,},
-	props: ['allProjects','category','description'],
-	data: () => {
-		return {
-			// remove this line
-			// FilteredCategory: description,
-		};
-	},
-	computed: {
-		FilteredCategory() {
-			return this.description;
-		},
-		projects() {
-			console.log(this.allProjects);
-			return this.allProjects;
-		},
-
-		// Get the filtered projects
-		filteredProjects() {
-			if (this.FilteredCategory) {
-				return this.filterProjectsByCategory();
-			} 
-			return this.projects;
-		},
-	},
-	methods: {
-		// Filter projects by category
-		filterProjectsByCategory() {
-			return this.projects.filter((item) => {
-				let category =
-					item.category.charAt(0).toUpperCase() +
-					item.category.slice(1);
-				console.log(category);
-				return category.includes(this.FilteredCategory);
-			});
-		},
-
-	},
-	mounted() {
-		feather.replace();
-	},
-};
-</script>
