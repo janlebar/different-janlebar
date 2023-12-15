8045956361aa3c9fdf85d3591c33bfae57120687
JAN <janstefanlebar@gmail.com>
2023-12-15 16:01:43 +0100

    UPDATE

--- pages/Indeximageload.vue
+++ pages/Indeximageload.vue
@@ -1,27 +1,17 @@
 <template>
-  <div>
-    <transition name="slide" @before-enter="beforeEnterLeft" @enter="enter">
-      <img
-        v-if="showLeftImage"
-        class="left-image"
-        :style="{ transform: `translateX(${hoverLeft * 10}%)` }"
-        @mousemove="updateHoverLeft"
-        @mouseleave="resetHoverLeft"
-        src="/img/canabiovet/canabiovet1.svg"
-        alt="Left Image"
-      />
-    </transition>
-    <transition name="slide" @before-enter="beforeEnterRight" @enter="enter">
-      <img
-        v-if="showRightImage"
-        class="right-image"
-        :style="{ transform: `translateX(${hoverRight * -10}%)` }"
-        @mousemove="updateHoverRight"
-        @mouseleave="resetHoverRight"
-        src="/img/canabiovet/canabiovet2.svg"
-        alt="Right Image"
-      />
-    </transition>
+  <div class="image-container">
+    <img
+      v-if="showLeftImage"
+      class="left-image"
+      src="/img/canabiovet/canabiovet1.svg"
+      alt="Left Image"
+    />
+    <img
+      v-if="showRightImage"
+      class="right-image"
+      src="/img/canabiovet/canabiovet2.svg"
+      alt="Right Image"
+    />
   </div>
 </template>
 
@@ -31,38 +21,7 @@ export default {
     return {
       showLeftImage: false,
       showRightImage: false,
-      hoverLeft: 0,
-      hoverRight: 0,
     };
-  },
-  methods: {
-    beforeEnterLeft(el) {
-      el.style.transform = "translateX(-100%)";
-    },
-    beforeEnterRight(el) {
-      el.style.transform = "translateX(100%)";
-    },
-    enter(el, done) {
-      el.offsetHeight; // Trigger reflow to apply transform immediately
-      el.style.transition = "transform 1s ease";
-      el.style.transform = `translateX(${this.hoverLeft * 10}%)`;
-      el.style.transform = `translateX(${this.hoverRight * -10}%)`;
-      done();
-    },
-    updateHoverLeft(event) {
-      const rect = event.target.getBoundingClientRect();
-      this.hoverLeft = (event.clientY - rect.top) / rect.height;
-    },
-    resetHoverLeft() {
-      this.hoverLeft = 0;
-    },
-    updateHoverRight(event) {
-      const rect = event.target.getBoundingClientRect();
-      this.hoverRight = (event.clientY - rect.top) / rect.height;
-    },
-    resetHoverRight() {
-      this.hoverRight = 0;
-    },
   },
   mounted() {
     // Use a timeout to stagger the appearance of the images
@@ -78,11 +37,19 @@ export default {
 </script>
 
 <style>
+.image-container {
+  position: relative;
+  width: 100%;
+  height: 100vh; /* Adjust this to fit your design */
+  overflow: hidden; /* Clip the images outside the container */
+}
+
 .left-image,
 .right-image {
   position: absolute;
   width: 25%;
-  transition: transform 0.5s ease; /* Add smooth transition for hover effect */
+  height: 100%;
+  top: 0;
 }
 
 .right-image {
@@ -90,8 +57,5 @@ export default {
 }
 </style>
 
-
-
-  
   
   
