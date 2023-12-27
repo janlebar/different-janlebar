0ba18d080daeed48ac87ed18e8efc67e0ce64a6d
JAN <janstefanlebar@gmail.com>
2023-12-27 20:51:12 +0100

    github

--- components/GitHubRepo.vue
+++ components/GitHubRepo.vue
@@ -1,32 +1,48 @@
 <!-- components/GitHubRepo.vue -->
 
 <template>
-    <div>
-      <h1 v-if="repo">{{ repo.name }}</h1>
-      <p v-if="repo">{{ repo.description }}</p>
-      <p v-if="repo">Stars: {{ repo.stargazers_count }}</p>
-      <p v-if="repo">Forks: {{ repo.forks_count }}</p>
-      <!-- Add more details as needed -->
-      <p v-if="!repo">Repository not found</p>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    async asyncData({ params, $http }) {
-      try {
-        // const response = await $http.get(`https://api.github.com/repos/${params.owner}/${params.repo}`);
-        const response = await $http.get('https://api.github.com/repos/janlebar/hanyman-connect');
-        return {
-          repo: response.data,
-        };
-      } catch (error) {
-        console.error('Error fetching GitHub repository:', error);
+  <div>
+    <h1 v-if="repo">{{ repo.name }}</h1>
+    <p v-if="repo">{{ repo.description }}</p>
+    <p v-if="repo">Stars: {{ repo.stargazers_count }}</p>
+    <p v-if="repo">Forks: {{ repo.forks_count }}</p>
+    <!-- Add more details as needed -->
+    <p v-if="!repo">Repository not found</p>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ params, $http }) {
+    try {
+      // Set your GitHub API token
+      const githubToken = 'YOUR_GITHUB_TOKEN';
+
+      // Include the token in the request headers
+      const response = await $http.get(`https://api.github.com/repos/${params.owner}/${params.repo}`, {
+        headers: {
+          Authorization: `Bearer ${githubToken}`,
+        },
+      });
+
+      // Check if the response status is 404 (Not Found)
+      if (response.status === 404) {
+        console.log('Repository not found:', params.owner, params.repo);
         return {
           repo: null,
         };
       }
-    },
-  };
-  </script>
-  
+
+      return {
+        repo: response.data,
+      };
+    } catch (error) {
+      console.error('Error fetching GitHub repository:', error);
+      return {
+        repo: null,
+      };
+    }
+  },
+};
+</script>
+
